1db19bbe1fe5553c72686e5e57bbeafa57e73a91
Francesco Debortoli <francescodebortoli2000@gmail.com>
2023-08-01 10:14:59 +0200

    algorith to add an edge

--- compiler/QTree.py
+++ compiler/QTree.py
@@ -31,7 +31,7 @@ class QTree:
     :type last: int
     """ 
     def __init__(self, head_id):
-        """Constructor method. When initializing a Quantum Tree"""
+        """Constructor method. When initializing a Quantum Tree we start with a qubit initialized in the state + (hence the Hadamard gate) and we label with this qubit with the integer index head_id"""
         self.loop = None
         self.qvertices = {}
         
@@ -45,19 +45,34 @@ class QTree:
     
         self.loop = Loop(photons=photons_from_qubit([head_qubit]), qbits=[head_qubit], circuit=circuit)
 
-        #keep in mind the last vertex that was added to the tree
+        #keep in mind the last vertex that was added to the tree. We will need this to sink qubits at the end of the circuit.
         self.last = head_id
     
 
     def sink(self, up, down):
+        """Sink the qubit with integer index up to the position of the qubit with integer position down.
+
+        :param up: position of the upper qubit
+        :type up: int
+        :param down: position of the lower qubit
+        :type down: int
+        """
         q_up = self.qvertices[up]
         q_down = self.qvertices[down]
 
         self.loop.sink(q_up, q_down) 
 
     def add_edge(self, parent, leaf, sink=True):
-        #print("ADDING EDGE", parent, leaf)
-        #the qbit parent must be already present in the tree
+        """Add an edge between the qubits with index parent and leaf. The qubit with index leaf will be newly created. The qbit parent must be already present in the tree.
+
+        :param parent: index of the parent qubit
+        :type parent: int
+        :param leaf: index of the leaf qubit
+        :type leaf: int
+        :param sink: you can decide to perform the fusion with or without sinking the parent qubit to the bottom of the circuit, defaults to True
+        :type sink: bool, optional
+        """
+
         #sink the parent vertex to the bottom (the last position), if they are different
         if(sink and (self.last != parent)):
             self.sink(parent, self.last)
@@ -70,11 +85,12 @@ class QTree:
 
         self.loop.fuse(qlost, self.qvertices[parent])
 
-        #put the parent at the end of the circuit
-        #self.sink(parent, leaf)
-        #self.last = parent
-
     def depth(self):
+        """Given two photonic lines representing a qubit, we can calculate in which outer loop the last optical element on this two lines is positioned.
+
+        :return: it returns a list mapping every qubit index to the corresponding outer loop number defined above.
+        :rtype: int
+        """
         anal = {}
         for v in self.qvertices:
             depth, last = self.loop.loopify(display=False)
@@ -86,48 +102,52 @@ class QTree:
             else:
                 d = max(ph, pv+1)
 
-
             d = ph
-
 
             if d==0:
                 d+=1
-
-
-            #print("vertex:", v, "depth:", d)
-            #print()
 
             anal[v] = d
 
         return anal
            
-        
 
+### this last two functions are not really useful. I used them to prove the correctness of the algorithm describing how the number of loops grows.
     def add_simul(self, parent, leaf, order, depth):
-                if order.index(parent) < len(order)-1:
-                    m = depth[order.index(parent)]
-                    for i in range(order.index(parent)+1, len(order)):
-                        m = max(m, depth[i]+1)
-                        depth[i-1] = m+1
-                        order[i-1] = order[i]
-                    order[len(order)-1] = parent
-                    depth[len(order)-1] = m+1
+        """This function simulates algorithmically the growth of the circuit when adding an edge.
 
-                depth[len(order)-1] += 1
+        :param parent: id of the parent qubit
+        :type parent: int
+        :param leaf: id of the leaf qubit
+        :type leaf: int
+        :param order: a list that keeps the order of the qubit in the photonic circuit (starting from above)
+        :type order: list of int
+        :param depth: a list recording the depth of every qubit's lines (as defined above)
+        :type depth: list of int
+        """
+        if order.index(parent) < len(order)-1:
+            m = depth[order.index(parent)]
+            for i in range(order.index(parent)+1, len(order)):
+                m = max(m, depth[i]+1)
+                depth[i-1] = m+1
+                order[i-1] = order[i]
+            order[len(order)-1] = parent
+            depth[len(order)-1] = m+1
 
-                order.append(leaf)
-                depth.append(1)
+        depth[len(order)-1] += 1
 
-                #bellurieee
-                anal = self.depth()
-                anal_sorted = []
+        order.append(leaf)
+        depth.append(1)
 
-                for i in range(0, len(anal)):
-                    anal_sorted.append(anal[order[i]])
-                #print("order\t", order)
-                #print("cdepth\t", anal_sorted)
-                #print("depth\t", depth)
+        anal = self.depth()
+        anal_sorted = []
+
+        for i in range(0, len(anal)):
+            anal_sorted.append(anal[order[i]])
+        #print("order\t", order)
+        #print("cdepth\t", anal_sorted)
+        #print("depth\t", depth)
 
     def add_overall(self, parent, leaf, order, depth):
-         self.add_edge(parent, leaf)
-         self.add_simul(parent, leaf, order, depth)
+        self.add_edge(parent, leaf)
+        self.add_simul(parent, leaf, order, depth)
