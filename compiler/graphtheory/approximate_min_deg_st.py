b415b30a9ba04b5d36a88cfde44e766c21dc4e8f
Korbinian Staudacher <Korbinian.Staudacher@nm.ifi.lmu.de>
2024-11-06 10:13:41 +0100

    approximate MDST, now with entirely correct algorithm

--- compiler/graphtheory/approximate_min_deg_st.py
+++ compiler/graphtheory/approximate_min_deg_st.py
@@ -37,87 +37,59 @@ def find_cycle(T, u, v):
 
     return None    
 
-def unblock_edge_candidate(G, st, F, k, u):
-    print("UNBLOCK EDGE CANDIDATE",u)
-    F_u = [F_i for F_i in F if u in F_i][0]
-    subgraph = nx.subgraph(G,F_u)
-    for v1, v2 in set(subgraph.edges()).difference(st.edges()):
-        if st.degree(v1) >= k-1 or st.degree(v2) >= k-1:
-            continue
-        else:
-            C = find_cycle(st,v1,v2)
-            if u in C:
-                st.add_edge(v1,v2)
-                print("unblock add edge",v1,v2)
-                neighbor_to_remove = set(C).intersection(set(st.neighbors(u))).pop()
-                print("unblock remove edge",u,neighbor_to_remove)
-                st.remove_edge(u, neighbor_to_remove)
-                if not nx.is_connected(st):
-                    import pdb
-                    pdb.set_trace()
-                break
+"""
+Thoughts: 
+If we remove all vertices with degree k and(!) k-1 in the first place, do we already find the correct improvements in the inner while loop?
+So we would not neet to "find" improvements afterwards but just apply the edge additions found in the inner while loop and delete some adjacent edges
+Can we remove any edge or does edge removal for a k-1 vertex lead to cycle breaking for the k vertex?
+Edge addition and removal inside a component preserves connectedness, therefore we always get a cycle when connecting two other components afterwards?
+"""
 
 def approximate_min_deg_st(G):
-    print("original graph",list(G.edges()))
-    st = nx.minimum_spanning_tree(G)
-    print("spanning tree edges",list(st.edges()))
-    while True:
-        marks = dict({node:'good' for node in st.nodes})
-        k = max(dict(st.degree).values())
-        for node in st.nodes():
-            if st.degree(node) >= k-1:
-                marks[node] = 'bad'
-        forest = nx.subgraph(st,[node for node in st.nodes if marks[node] == 'good'])
+    T = nx.minimum_spanning_tree(G)
+    while True: 
+        k = max(dict(T.degree).values())
+        marks = dict({node:'good' if T.degree(node) < k-1 else 'bad' for node in T.nodes})
+        forest = nx.subgraph(T,[node for node in T.nodes if marks[node] == 'good'])
         F = list(nx.connected_components(forest))
-        # print("connected components",F)
-        k_candidate = None
-        # print("bad nodes for k=",k,":",marks)
-        print("degree dict",dict(st.degree))
-        while all([True if marks[node] == 'bad' else False for node in st.nodes if st.degree(node) == k]):
+        edges_to_apply = dict()
+        while True:
             edge = get_edge_between_components(G,F)
-            if not edge:
+            if not (edge and all([True if marks[node] == 'bad' else False for node in T.nodes if T.degree(node) == k])):
                 break
             u,v = edge
-            # st.add_edge(u,v)
-            # print("add edge",u,v)
-            C = find_cycle(st,u,v)
-            if not C:
-                import pdb
-                pdb.set_trace()
+            C = find_cycle(T, u, v)
             for node in C:
-                if st.degree(node) >= k-1 and marks[node] == 'bad':
+                if marks[node] == 'bad':
                     marks[node] = 'good'
-                    if st.degree(node) == k:
-                        #this is where we really find a candidate ending the while loop
-                        k_candidate = (node, (u,v), C)
-            forest = nx.subgraph(st,[node for node in st.nodes if marks[node] == 'good'])
+                    edges_to_apply[node] = (u,v)
+            forest = nx.subgraph(T,[node for node in T.nodes if marks[node] == 'good'])
             F = list(nx.connected_components(forest))
-        
-        if k_candidate:
-            candidate, (u,v), circle = k_candidate
-            if st.degree(u) >= k-1:
-                # print("degree u",st.degree(u),"k",k)
-                unblock_edge_candidate(G, st, F, k, u)
+
+        good_k_vertices = [node for node in T.nodes if marks[node] == 'good' and T.degree(node) == k]
+        if good_k_vertices:
+            k_vertex = good_k_vertices[0]
+            u,v = edges_to_apply[k_vertex]
+            if T.degree(u) == k-1:
+                uu, uv = edges_to_apply[u]
+                C = find_cycle(T, uu, uv)
+                T.add_edge(uu, uv)
+                T.remove_edge(u, set(C).intersection(set(T.neighbors(u))).pop())
+            if T.degree(v) == k-1:
+                vu, vv = edges_to_apply[v]
+                C = find_cycle(T, vu, vv)
+                T.add_edge(vu, vv)
+                T.remove_edge(v, set(C).intersection(set(T.neighbors(v))).pop())
             
-            if st.degree(v) >= k-1:
-                # print("degree v",st.degree(v),"k",k)
-                unblock_edge_candidate(G, st, F, k, v)
-            
-            print("reduce degree of vertex ",candidate,"with neighbors",list(st.neighbors(candidate)),"by introducing an edge on",u,v)
-            st.add_edge(u,v)
-            neighbor_to_remove = set(circle).intersection(set(st.neighbors(candidate))).pop()
-            print("remove edge",candidate, neighbor_to_remove)
-            st.remove_edge(candidate, neighbor_to_remove)
-            if not nx.is_connected(st):
-                import pdb
-                pdb.set_trace()
-            print("new spanning tree",list(st.edges()))
+            C = find_cycle(T, u,v)
+            T.add_edge(u,v)
+            T.remove_edge(k_vertex, set(C).intersection(set(T.neighbors(k_vertex))).pop())
         else:
             break
-    assert(nx.is_connected(st))
-    assert(len(st.edges()) == len(st.nodes())-1)
-    assert(all([True if edge in G.edges() else False for edge in st.edges()]))
-    return st
+        assert(nx.is_connected(T))
+        assert(len(T.edges()) == len(T.nodes())-1)
+        assert(all([True if edge in G.edges() else False for edge in T.edges()]))
+    return T
 
 if __name__ == "__main__":
     random.seed(1234)
