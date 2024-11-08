c04d94f2a28022f5fb2e62642899c922c02e9d3a
BillyJay <billa.j@gmail.com>
2024-11-08 16:09:39 +0100

    output of times as csv file

--- First Passage/get_F.py
+++ First Passage/get_F.py
@@ -2,9 +2,14 @@ import string
 import numpy as np
 import sys
 from numpy.linalg import matrix_power
+import csv
+import sys
+
+
 
 file = open('matrix.txt')
 
+jsonFileName = sys.argv[1] 
 mat_orig = []
 for line in file:
 	mat_orig.append(line.split())
@@ -16,8 +21,9 @@ divs = 0
 for i in range(dim):
 	if(mat_orig[i][dim-1]!="x"):
 		divs += 1
-
-for p in np.linspace(0.1,1.0, 20):
+allData = []
+pvals = [0.1, 0.5, 0.75, 0.8, 0.9,0.95,1.0]
+for p in pvals:
 	# print(p)
 	mp = {}
 	mp['1'] = 1.0
@@ -39,9 +45,6 @@ for p in np.linspace(0.1,1.0, 20):
 			mat[i][dim-2] = 0.0
 			mat[i][dim-1] = 0.0
 
-
-	 
-
 	mat = np.array(mat)
 	# np.set_printoptions(threshold=sys.maxsize)
 	E = np.full((dim,dim),1.0)
@@ -59,3 +62,10 @@ for p in np.linspace(0.1,1.0, 20):
 	Z0 = np.diag(np.diag(Z))
 	F = np.matmul(Dmat,np.identity(dim)-Z+np.matmul(Z0,E))
 	print("{} {}".format(p,F[dim-3][0]))
+	allData.append(F[dim-3][0])
+
+
+csvfile = open('FP_data.csv', 'a')
+spamwriter = csv.writer(csvfile, delimiter=';')
+spamwriter.writerow([jsonFileName, *pvals])
+spamwriter.writerow([jsonFileName, *allData])
